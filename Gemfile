dcfcf61aaad9d4bcf902b8b016958c1e65c71115
pjskennedy <peterjskennedy@gmail.com>
2018-12-07 15:46:15 -0800

    Added ruby skeleton project

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'logger'
+
+group :test, :development do
+  gem 'pry'
+  gem 'rake'
+  gem 'rspec'
+  gem 'rubocop', require: false
+end
---
+++ main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+$LOAD_PATH << File.dirname(__FILE__)
+$LOAD_PATH << File.join('lib')
+
+# Deps
+require 'logger'
+
+# Libs
+
+if $PROGRAM_NAME == __FILE__
+  # Program execution goes here
+
+  exit 0
+end
